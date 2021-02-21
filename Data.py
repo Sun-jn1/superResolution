15988bc1a4c0abae4c7ab0482e786ea974c628be
Sun-jn1 <894417322@qq.com>
2021-02-21 19:09:21 +0800

    newData

--- Data.py
+++ Data.py
@@ -65,3 +65,60 @@ train_data,test_data = random_split(data,[train_size,test_size])
 train_loader = DataLoader(train_data,batch_size=4,shuffle=True)
 test_loader = DataLoader(test_data,batch_size=4,shuffle=True)
 
+class Data(Dataset):
+    def __init__(self):
+        file_path = "data"
+        symbol="\\"
+        self.ouput = []
+        self.inputs = []
+        data_transform = transforms.Compose(transforms=[
+            transforms.Resize((32,32)),
+
+            transforms.ToTensor(),
+
+            ])
+        data_transform1 = transforms.Compose(transforms=[
+            transforms.Resize((128,128)),
+
+            transforms.ToTensor(),
+
+        ])
+
+        for root, dirs, files in os.walk(file_path):
+            int = 0
+
+            for file in files:
+                int+=1
+                if int<100 and int%10==0:
+                    img=Image.open(root + symbol + file)
+                    img=data_transform1(img)
+                    img=np.array(img,dtype="float")
+                    self.ouput.append(img)
+
+
+
+        for root, dirs, files in os.walk(file_path):
+            int = 0
+            for file in files:
+                int += 1
+                if int < 100 and int % 10 == 0:
+                    img=Image.open(root + symbol + file)
+                    img = data_transform(img)
+                    img = np.array(img,dtype="float")
+                    self.inputs.append(img)
+        self.len = len(self.ouput)
+        self.ouput = np.array(self.ouput, dtype="float")
+        self.inputs = np.array(self.inputs,dtype="float")
+
+
+    def __getitem__(self, item):
+        return torch.FloatTensor(self.inputs[item]),torch.FloatTensor(self.ouput[item])
+
+    def __len__(self):
+        return self.len
+pdata=Data()
+pdata = DataLoader(pdata,batch_size=4)
+
+
+
+
